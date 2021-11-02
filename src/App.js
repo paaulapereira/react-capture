c0d6f4aa2647599835fb0780d7ea9ef4b6575d47
paula_pereira <paula.decosta@alumnos.uneatlantico.es>
2021-11-02 17:05:29 +0100

    router

--- src/App.js
+++ src/App.js
@@ -3,16 +3,33 @@ import React from "react";
 import GlobalStyle from "./components/GlobalStyle";
 //import pages
 import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+//import components
 import Nav from "./components/Nav";
+//router
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
-
+//el rout detecta el camino y busca lo que haya en el path, el exact es para que sea exactamente igual a la ruta
+//el switch busca en la ruta la primera solucion posible y va cambiando
+//El componente route: el path en route es para escoger la ruta que va a renderizar cada componente
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+//el browser router es para escoger cual parte de la pagina vamos a renderizar y no todo a la vez
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
