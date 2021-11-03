0ba6dfa8a5a845677d9cae22f76e12d587fea6f9
paula_pereira <paula.decosta@alumnos.uneatlantico.es>
2021-11-03 14:28:00 +0100

    animaciones en los movies

--- src/animation.js
+++ src/animation.js
@@ -68,12 +68,20 @@ export const sliderContainer = {
   hidden: { opacity: 1 },
   show: { opacity: 1, transition: { staggerChildren: 0.15, ease: "easeOut" } },
 };
-
+//animacion para cuando haga scroll en la pagina
 export const scrollReveal = {
   hidden: { opacity: 0, scale: 1.2, transition: { duration: 0.5 } },
   show: {
     opacity: 1,
     scale: 1,
     transition: { duration: 0.5 },
+  },
+};
+
+export const swoopAdoop = {
+  hidden: { width: "0%", transition: { duration: 1 } },
+  show: {
+    width: "100%",
+    transition: { ease: "easeOut", duration: 1 },
   },
 };
